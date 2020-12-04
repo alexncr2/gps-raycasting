49d044b142a375fe50cdb4425c4af584f792708a
Alex Nicoara <alex.nicoara3@gmail.com>
2020-12-04 22:00:32 +0200

    Implement raycasting on multiple walls

--- gps-raycasting/src/components/CustomSketch.js
+++ gps-raycasting/src/components/CustomSketch.js
@@ -1,63 +1,111 @@
 import React, { useState } from "react";
 import Sketch from "react-p5";
 import PropTypes from "prop-types";
+import { Vector } from "p5";
 
-const CustomSketch = ({ line, ray }) => {
+const CustomSketch = ({ lines }) => {
 
-  // boundary
-  const [a, setA] = useState(null);
-  const [b, setB] = useState(null);
+  // boundary (wall)
+  const [walls, setWalls] = useState([]);
+  const [xoff, setXoff] = useState(0);
+  const [yoff, setYoff] = useState(1000);
 
-  // ray
-  const [pos, setPos] = useState(null);
-  const [dir, setDir] = useState(null);
+  const addWall = (aVector, bVector) => {
+    setWalls(walls => [...walls, { a: aVector, b: bVector }]);
+  };
 
-  const castRayOnWall = (p5) => {
-    const x1 = a.x;
-    const y1 = a.y;
-    const x2 = b.x;
-    const y2 = b.y;
-  
-    const x3 = pos.x;
-    const y3 = pos.y;
-    const x4 = pos.x + dir.x;
-    const y4 = pos.y + dir.y;
-  
+  // particle
+  const [particle, setParticle] = useState(null);
+
+  const castRayOnWalls = (p5, ray, wall) => {
+    const x1 = wall.a.x;
+    const y1 = wall.a.y;
+    const x2 = wall.b.x;
+    const y2 = wall.b.y;
+
+    const dir = Vector.fromAngle(ray.angle);
+
+    const x3 = particle.pos.x;
+    const y3 = particle.pos.y;
+    const x4 = particle.pos.x + dir.x;
+    const y4 = particle.pos.y + dir.y;
+
     const den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
     if (den === 0) {
       return;
     }
-  
+
     const t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / den;
     const u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / den;
-  
+
     if (t > 0 && t < 1 && u > 0) {
       let pt = p5.createVector();
       pt.x = x1 + t * (x2 - x1);
       pt.y = y1 + t * (y2 - y1);
       return pt;
     }
-  
+
     return;
   }
 
-  const lootAtPoint = (x, y) => {
-    dir.x = x - pos.x;
-    dir.y = y - pos.y;
-    dir.normalize();
+  const lookAtWalls = (p5) => {
+    for (let ray of particle.rays) {
+      let record = Infinity;
+      let closestPoint = null;
+      for (let wall of walls) {
+        const pt = castRayOnWalls(p5, ray, wall);
+        if (pt) {
+          const distance = Vector.dist(particle.pos, pt);
+          // distance = min(distance, record);
+          if (distance < record) {
+            record = distance;
+            closestPoint = pt;
+          }
+        }
+      }
+      if (closestPoint) {
+        p5.stroke(255, 69);
+        p5.line(particle.pos.x, particle.pos.y, closestPoint.x, closestPoint.y);
+      }
+    }
+  }
+
+  const createRays = (p5, pos) => {
+    let rays = [];
+    // change angle increment for more rays
+    for (let angle = 0; angle < 360; angle += 1) {
+      rays.push({
+        pos: pos,
+        angle: p5.radians(angle)
+      });
+    }
+    return rays;
+  }
+
+  const updateParticlePosition = (x, y) => {
+    particle.pos.set(x, y);
   }
 
   const setup = (p5, canvasParentRef) => {
-    // boundary
-    setA(p5.createVector(line.x1, line.y1));
-    setB(p5.createVector(line.x2, line.y2));
+    const canvasWidth = window.innerWidth;
+    const canvasHeight = window.innerHeight;
 
-    // ray
-    setPos(p5.createVector(ray.x, ray.y));
-    setDir(p5.createVector(1, 0));
+    for (let line of lines) {
+      const a = p5.createVector(line.x1, line.y1);
+      const b = p5.createVector(line.x2, line.y2);
+      addWall(a, b);
+    }
+
+    // particle
+    const particlePosition = p5.createVector(canvasWidth / 2, canvasHeight / 2);
+
+    setParticle({
+      pos: particlePosition,
+      rays: (createRays(p5, particlePosition))
+    });
 
     // sketch
-    p5.createCanvas(600, 600).parent(canvasParentRef);
+    p5.createCanvas(canvasWidth, canvasHeight).parent(canvasParentRef);
   };
 
   const draw = (p5) => {
@@ -65,22 +113,33 @@ const CustomSketch = ({ line, ray }) => {
     p5.background(0);
 
     // boundary
-    p5.stroke(255);
-    p5.line(a.x, a.y, b.x, b.y);
-
-    // ray
-    p5.stroke(255);
-    p5.push();
-    p5.translate(pos.x, pos.y);
-    p5.line(0, 0, dir.x * 10, dir.y * 10);
-    p5.pop();
-    lootAtPoint(p5.mouseX, p5.mouseY);
-
-    let point = castRayOnWall(p5);
-    if (point) {
-      p5.fill(255);
-      p5.ellipse(point.x, point.y, 8, 8);
+    for (let wall of walls) {
+      p5.stroke(255);
+      p5.line(wall.a.x, wall.a.y, wall.b.x, wall.b.y);
     }
+
+    // particle - show
+    p5.fill(255);
+    p5.ellipse(particle.pos.x, particle.pos.y, 4);
+    for (let ray of particle.rays) {
+      // ray - show
+      p5.stroke(255);
+      p5.push();
+      p5.translate(ray.pos.x, ray.pos.y);
+      const dir = Vector.fromAngle(ray.angle);
+      p5.line(0, 0, dir.x * 10, dir.y * 10);
+      p5.pop();
+    }
+
+    // particle move with mouse or perlin noise
+    updateParticlePosition(p5.mouseX, p5.mouseY);
+    //updateParticlePosition(p5.noise(xoff) * window.innerWidth, p5.noise(yoff) * window.innerHeight);
+
+    setXoff(xoff + 0.01);
+    setYoff(yoff + 0.01);
+
+    // particle look at wall
+    lookAtWalls(p5);
   };
 
   return (
@@ -91,14 +150,12 @@ const CustomSketch = ({ line, ray }) => {
 export default CustomSketch;
 
 CustomSketch.propTypes = {
-  line: PropTypes.shape({
-    x1: PropTypes.number,
-    y1: PropTypes.number,
-    x2: PropTypes.number,
-    y2: PropTypes.number
-  }),
-  ray: PropTypes.shape({
-    x: PropTypes.number,
-    y: PropTypes.number,
-  })
+  lines: PropTypes.arrayOf(
+    PropTypes.shape({
+      x1: PropTypes.number,
+      y1: PropTypes.number,
+      x2: PropTypes.number,
+      y2: PropTypes.number
+    })
+  )
 };
--- gps-raycasting/src/components/RayCasting.js
+++ gps-raycasting/src/components/RayCasting.js
@@ -2,21 +2,57 @@ import React from 'react';
 import CustomSketch from './CustomSketch';
 
 function RayCasting(props) {
-  const line = {
-    x1: 300,
-    y1: 100,
-    x2: 300,
-    y2: 500
-  };
 
-  const ray = {
-    x: 100,
-    y: 200
-  };
+  const getRandomWidth = () => {
+    return Math.floor(Math.random() * window.innerWidth) + 1;
+  }
+
+  const getRandomHeight = () => {
+    return Math.floor(Math.random() * window.innerHeight) + 1;
+  }
+
+  const getRandomLines = (count) => {
+    // initialized with border
+    let lines = [
+      {
+        x1: 0,
+        y1: 0,
+        x2: window.innerWidth,
+        y2: 0,
+      }, // top 
+      {
+        x1: window.innerWidth,
+        y1: 0,
+        x2: window.innerWidth,
+        y2: window.innerHeight,
+      }, // right
+      {
+        x1: window.innerWidth,
+        y1: window.innerHeight,
+        x2: 0,
+        y2: window.innerHeight,
+      }, // bottom
+      {
+        x1: 0,
+        y1: window.innerHeight,
+        x2: 0,
+        y2: 0,
+      }, // left
+    ];
+
+    for (let i = 0; i < count; i++) {
+      lines = [...lines, {
+        x1: getRandomWidth(), y1: getRandomHeight(),
+        x2: getRandomWidth(), y2: getRandomHeight()
+      }];
+    }
+
+    return lines;
+  }
 
   return (
     <div>
-      <CustomSketch line={line} ray={ray} />
+      <CustomSketch lines={getRandomLines(5)} />
     </div>
   );
 }
